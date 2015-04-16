6ec588892b76137b4b63b233b93fd5c9e95b725c
Max North <maxwelldnorth@gmail.com>
2015-04-16 13:15:15 -0400

    4/16/20

--- InputXTypes.cs
+++ InputXTypes.cs
@@ -29,7 +29,7 @@ public enum InputCode
 	//AltGr = KeyCode.AltGr, Help = KeyCode.Help, Print, SysReq, Break, Menu, 
 	
 	
-	Mouse0 = KeyCode.Mouse0, Mouse1 = KeyCode.Mouse1, Mouse2 = KeyCode.Mouse2, 
+	//Mouse0 = KeyCode.Mouse0, Mouse1 = KeyCode.Mouse1, Mouse2 = KeyCode.Mouse2, 
 	//Mouse3 = KeyCode.Mouse3, Mouse4 = KeyCode.Mouse4, Mouse5 = KeyCode.Mouse5, Mouse6 = KeyCode.Mouse6, 
 	
 	/*
